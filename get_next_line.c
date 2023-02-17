35702a891831654160edc0219d66d246440ac3f7
jinsyang <jinsyang@student.42seoul.kr>
2023-02-17 17:48:56 +0900

    fucking norm2

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,23 @@
 /*   By: jinsyang <jinsyang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 16:48:07 by jinsyang          #+#    #+#             */
-/*   Updated: 2023/02/17 16:05:53 by jinsyang         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:40:11 by jinsyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// stay 유무에 따라 reulst 에 복사 아니면 buf에 복사
+// 용도에 따라 함수 나누기
+// 아니면 처음 다시 구성부터 갈아엎기
+
+void free_stay(char *stay)
+{
+	free(stay);
+}
+
 char	*get_next_line2(char *result, int fd, int fd_index, int result_len)
-{ // 42line 하나 함수 더 만들어서 나누기 ㄱ..그만
+{ // 25line 하나 함수 더 만들어서 나누기 ㄱ..그만
 	char		buf[BUFFER_SIZE];
 	static char	*stay;
 	int			index;
@@ -22,13 +31,13 @@ char	*get_next_line2(char *result, int fd, int fd_index, int result_len)
 	flag = 1;
 	while (flag)
 	{
-		if (stay)
+		if (stay) //buf가 변경안된다면 차라리 다 비우고 넣
 			while (stay[++fd_index])
 				buf[fd_index] = stay[fd_index];
 		else
 			fd_index = read(fd, buf, BUFFER_SIZE);
-		free(stay); // !!!
-		stay = NULL; // !!!
+		free_stay(stay); // !!! free 자체는 다른 함수 X
+		stay = NULL; // !!! 이게 문제였던
 		if (fd_index == 0)
 			return (result);
 		else if (fd_index < 0)
